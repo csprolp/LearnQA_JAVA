b4e89342a87e35e258ff8ead20865fc36e50f0f0
Vyacheslav <csprolp@gmail.com>
2023-10-13 22:22:08 +0300

    third Task from second homework

--- src/test/java/SecondLessonTests.java
+++ src/test/java/SecondLessonTests.java
@@ -1,6 +1,9 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLOutput;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -16,5 +19,41 @@ public class SecondLessonTests {
          System.out.println(secondMessage);
     }
 
+    @Test
+    public void redirectLocation(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        System.out.println((response.getStatusCode()));
+        System.out.println(response.getHeader("location"));
+    }
 
+    @Test
+    public void longRedirectLocation() {
+        int i = 0;
+        Response response;
+        String link = "https://playground.learnqa.ru/api/long_redirect";
+        do {
+
+            response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(link)
+
+                    .andReturn();
+            i++;
+            System.out.println((response.getStatusCode()));
+            System.out.println(response.getHeader("location"));
+            link = response.getHeader("location");
+            System.out.println(i);
+        }
+        while (response.statusCode() != 200);
+
+    }
 }
