3ffc125bc0cd30904d81928c262086cb807d30e8
Vyacheslav <csprolp@gmail.com>
2023-10-23 21:51:11 +0300

    Fourth Task from third homework

--- src/test/java/ThirdLessonTests.java
+++ src/test/java/ThirdLessonTests.java
@@ -1,8 +1,10 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
@@ -47,6 +49,31 @@ public class ThirdLessonTests {
         } else {
             throw new IllegalArgumentException("We don't have this condition in HW");
         }
+    }
+    @ParameterizedTest
+    @CsvSource(
+            {
+                    "'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1', Mobile, Chrome, iOS",
+                    "'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)', Googlebot, Unknown, Unknown",
+                    "'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0', Web, Chrome, No",
+                    "'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1', Mobile, No, iPhone",
+                    "'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30', Mobile, No, Android"
+            }
+
+
+    )
+    public void fourthTest(String userAgent, String expectedPlatform, String  expectedBrowser, String expectedDevice){
+        Map<String, String> cookies = new HashMap<>();
+        cookies.put("User-Agent", userAgent);
+        JsonPath response = RestAssured
+                .given()
+                .headers(cookies)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+        response.prettyPrint();
+        assertTrue(response.getString("platform").equals(expectedPlatform), "Platform is incorrect for " + userAgent);
+        assertTrue(response.getString("browser").equals(expectedBrowser), "Browser is incorrect for " + userAgent);
+        assertTrue(response.getString("device").equals(expectedDevice), "Device is incorrect for " + userAgent);
     }
 
 
